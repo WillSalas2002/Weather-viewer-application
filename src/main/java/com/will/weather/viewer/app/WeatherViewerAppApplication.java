2d641e0e5e13ef646988b17a10b6b31b65b6520b
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-03 09:43:28 +0500

    First commit

---
+++ src/main/java/com/will/weather/viewer/app/WeatherViewerAppApplication.java
@@ -0,0 +1,13 @@
+package com.will.weather.viewer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherViewerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherViewerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/weather/viewer/app/WeatherViewerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.weather.viewer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherViewerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
