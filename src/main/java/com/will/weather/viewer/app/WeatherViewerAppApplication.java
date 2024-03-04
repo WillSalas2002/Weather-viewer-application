b91c6865635ebfd88c2276c7f31fcd8056fe3668
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-04 09:20:10 +0500

    Create RestTemplate bean

--- src/main/java/com/will/weather/viewer/app/WeatherViewerAppApplication.java
+++ src/main/java/com/will/weather/viewer/app/WeatherViewerAppApplication.java
@@ -2,6 +2,8 @@ package com.will.weather.viewer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WeatherViewerAppApplication {
@@ -10,4 +12,8 @@ public class WeatherViewerAppApplication {
 		SpringApplication.run(WeatherViewerAppApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
